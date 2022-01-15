22ec6046a79bb2239ea0096eb7c15ba9d735e486
Dashevichartem1991 <dashevichartem@gmail.com>
2022-01-15 23:31:29 +0300

    change information

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,8 +1,10 @@
 public class helloworld {
 
     public static void main(String [] args) {
-
-        System.out.println("Hello World");
+        String name ="My name is Artem,";
+        String age =" I'm 30 years old, ";
+        String place ="I live in Minsk.";
+        System.out.println(name+age+place);
     }
 
 }
